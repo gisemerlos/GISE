4d347627655d515834a2b5c601f0ead82bd22aff
Lucas Licantica <Lucasmaxi05@mail.com>
2023-08-31 18:26:53 -0300

    ja

--- src/updateIPAddress.ts
+++ src/updateIPAddress.ts
@@ -1,8 +1,11 @@
 import { Contacto } from "../src/contactos";
- 
-const updateIPAddress =(contacto: Contacto)=> {
 
-    
+const updateIPAddress = (contactos: Contacto[]) => {
+    contactos.forEach(contacto => {
+        contacto.ip_address = contacto.ip_address + "." + contacto.id
+    });
+    return contactos
+
 }
 
-export {updateIPAddress}
+export { updateIPAddress }
